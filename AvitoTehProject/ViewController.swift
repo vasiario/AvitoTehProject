2f446cf82aad23b226723456da8e2adfc9ce638b
vasiario <vasiariov@gmail.com>
2023-08-25 17:26:02 +0200

    Added project

---
+++ AvitoTehProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTehProject
+//
+//  Created by vasiario on 25.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
